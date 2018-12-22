b7eea1eb16ebe12b443b92f654a94a8f012c24a0
asim ismail <asimismailuofa@yahoo.com>
2018-12-22 13:31:52 -0700

    Removing Testing

--- server.js
+++ server.js
@@ -22,5 +22,3 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglis
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-
-Testing
