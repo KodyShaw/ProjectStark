99a1a853f21504771d829317506970f11f967312
KodyShaw <kodyshaw16@gmail.com>
2018-12-22 11:19:53 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ProjectStark
-Who even  reads theses things anyways?
+
+initalize with "npm install"
+
+install dev dependancies with "npm install --only=dev"
+
+start server/app with "npm run start"
+
+Keep service worker? Only deploy it right before we deploy.
---
+++ routes/api/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+
+
+
+module.exports = router;
+
+
+//You can create new routes to our own API here if we use it. 
+//Look at last weeks react class asignments for examples. 
+//we can add or remove to these folders as we want
