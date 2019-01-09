13d57fbea5c026dcfcf7aa1e9bb5975a32dd1b07
HeyyItsMike <mikewylie148@gmail.com>
2019-01-08 21:13:44 -0700

    mdbliabry

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, NavbarToggler, Collapse, Dropdown, DropdownToggle, DropdownMenu,  DropdownItem, Fa } from "mdbreact";
+
+class NavbarPage extends React.Component {
+  state = {
+    isOpen: false
+  };
+
+  toggleCollapse = this.setState({ isOpen: !this.state.isOpen });
+
+  render() {
+    return (
+      <Navbar color="default-color" dark expand="md" style={{marginTop: "20px"}}>
+          <NavbarBrand>
+            <strong className="white-text">Navbar</strong>
+          </NavbarBrand>
+          <NavbarToggler
+            onClick={this.toggleCollapse}
+          />
+          <Collapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+            <NavbarNav left>
+              <NavItem active>
+                <NavLink to="#!">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="#!">Features</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="#!">Pricing</NavLink>
+              </NavItem>
+              <NavItem>
+                <Dropdown>
+                  <DropdownToggle  nav caret>
+                    <div className="d-none d-md-inline">Dropdown</div>
+                  </DropdownToggle>
+                  <DropdownMenu className="dropdown-default"right>
+                    <DropdownItem href="#!">Action</DropdownItem>
+                    <DropdownItem href="#!">Another Action</DropdownItem>
+                    <DropdownItem href="#!">Something else here</DropdownItem>
+                    <DropdownItem href="#!">Something else here</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </NavItem>
+            </NavbarNav>
+            <NavbarNav right>
+              <NavItem>
+                <NavLink className="waves-effect waves-light" to="#!"><Fa icon="twitter" /></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="waves-effect waves-light" to="#!"><Fa icon="google-plus" /></NavLink>
+              </NavItem>
+              <NavItem>
+                <Dropdown>
+                  <DropdownToggle nav caret>
+                    <Fa icon="user" />
+                  </DropdownToggle>
+                  <DropdownMenu className="dropdown-default" right>
+                    <DropdownItem href="#!">Action</DropdownItem>
+                    <DropdownItem href="#!">Another Action</DropdownItem>
+                    <DropdownItem href="#!">Something else here</DropdownItem>
+                    <DropdownItem href="#!">Something else here</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </NavItem>
+            </NavbarNav>
+          </Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default NavbarPage;
