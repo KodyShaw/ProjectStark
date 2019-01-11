d8de1f9b88cfaa4d7cbb29469b1bb643de68304a
KodyShaw <kodyshaw16@gmail.com>
2019-01-10 19:21:01 -0700

    files in wrong place. fixed

--- client/utils/api/axios-instance.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-//Can add other axios.create Intances here to save code in long run
-//headers might change slightly based om the API used
-export default {
-    baseAPI: axios.create({
-        baseURL: `${__dirname}/api`,
-    })
-}
